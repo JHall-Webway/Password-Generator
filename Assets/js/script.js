fc227d94f1e1ac485a1a596bb036565faffd1b81
JHallUofA <83625694+JHallUofA@users.noreply.github.com>
2021-07-04 16:36:52 -0700

    added charset options

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,6 +9,42 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// generate password
+function generatePassword() {
 
+  var charset = ""
+
+  // Password length
+  var length = parseInt(window.prompt("How long do you want your password to be? (min: 8, max: 128)"))
+  //check to see if length input is a number
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a number within parameters.")
+    generatePassword();
+  } else {
+  console.log("Password Length = " + length);
+  }
+
+  var lowerCase = window.confirm("Do you want lower case?");
+  if (lowerCase) {
+    charset = charset + "abcdefghijklmnopqrstuvwxyz"
+  }
+
+  var upperCase = window.confirm("Do you want upper case?");
+  if (upperCase) {
+    charset = charset + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+
+  var numeric = window.confirm("Do you want numbers?");
+  if (numeric) {
+    charset = charset + "1234567890"
+  }
+
+  var specialChar = window.confirm("Do you want special characters?");
+  if (specialChar) {
+    charset = charset + "!#$%&()*+-,/:;<=>?@[]^_`{}|~"
+  }
+
+  console.log(charset);
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
